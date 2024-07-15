4f04d84846d8f2c2b46f1c6fc7a30e2eb5c9b715
Tanishka <156530628+samplertagithub@users.noreply.github.com>
2024-07-15 19:43:48 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="tanishka@google.com"
+var accountPassword="12345"
+accountCity="Pune"
+let accountState;
+
+// accountId=2 // not allowed
+
+
+
+
+accountEmail="Tc@tc.com"
+accountPassword="212121212"
+accountCity="Mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // we are using node js,not browser
+
+console.log(3+3)
+
+// console.log("Tanishka")
+
+let name="Tanishka"
+let age=21
+let isLoggedIn=false
+
+// primitive datatypes
+// number=> 2 to power of 53
+//bigint=>for bigger number
+//string=>""
+// bollean=> true/false
+// null=> standalone value hai
+// undefined=> when u havent assigned valued
+// symbol=> to find uniqueness 
+
+// object
+
+console.log(typeof "Monal");
+console.log(typeof age);
+console.log(typeof null );
+console.log(typeof undefined );
+
